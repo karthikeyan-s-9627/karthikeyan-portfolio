7ce780e1d4999f7fe32de89d2b319e2a04709592
[dyad] <git@dyad.sh>
2025-07-19 15:33:03 +0530

    [dyad] Standardizing project card image height - wrote 1 file(s)

--- src/components/ProjectsSlider.tsx
+++ src/components/ProjectsSlider.tsx
@@ -30,7 +30,7 @@ const ProjectsSlider: React.FC<ProjectsSliderProps> = ({ projects }) => {
           <div className="project-slide" key={index}>
             <Card className="h-full bg-card/80 backdrop-blur-sm border border-border/50 rounded-xl overflow-hidden flex flex-col
               shadow-[0_20px_40px_-10px_rgba(0,0,0,0.6)] dark:shadow-[0_20px_40px_-10px_rgba(0,0,0,0.3)] hover:shadow-[0_30px_60px_-15px_hsl(var(--primary)/0.6)] hover:scale-[1.02] transition-all duration-300">
-              <div className="relative h-[60%]">
+              <div className="relative h-[250px]">
                 <img
                   src={project.image}
                   alt={project.title}
